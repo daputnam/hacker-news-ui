d38dec435867df30376996c20253a7cf2f4ee1b2
Dan Putnam <daniel.a.putnam@gmail.com>
2018-05-07 23:58:04 -0500

    structure and some css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class HackerNews extends Component {
+  render() {
+    return (
+      <div className="main">
+        title bar here
+        <div>
+          list of newsItems here
+        </div>
+      </div>
+    );
+  }
+}
+const data = [
+  {
+    title: "Why I'm suing the government",
+    website: "www.bunniestudios.com",
+    points: 1233,
+    userName: "Ivank",
+    comments: 121
+  },
+  {
+    title: "Why I'm suing the government",
+    website: "www.bunniestudios.com",
+    points: 1233,
+    userName: "Ivank",
+    comments: 121
+  }
+];
+ReactDOM.render(<HackerNews />, document.getElementById('root'));
 
